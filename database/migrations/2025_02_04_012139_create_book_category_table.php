07006df471c638f4d047fa5854ded9848e552952
NymWidana <widan0001@gmail.com>
2025-02-04 09:45:23 +0800

    feat(migrations): update table columns and create pivot tables
    - Added new columns to the 'books', 'categories' and 'authors' tables:
      - 'books' table: Added 'title' (string) and 'description' (text) columns
      - 'categories' table: Added 'name' (string) columns
      - 'author' table: Added 'name' (string) columns
    - Created a pivot table migration file for the many-to-many relationship between 'books' and 'categories':
      - Migration file for 'book_category' table
      - Foreign key constraints for 'book_id' and 'category_id'
    - Created a pivot table migration file for the many-to-many relationship between 'authors' and 'books':
      - Migration file for 'author_book' table
      - Foreign key constraints for 'author_id' and 'book_id'
    
    These changes update the 'books', 'categories' and 'authors' tables with new columns and establish the necessary pivot table for managing the relationship between 'books' with 'categories', and 'authors' with 'books'

--- database/migrations/2025_02_04_012139_create_book_category_table.php
+++ database/migrations/2025_02_04_012139_create_book_category_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('book_category', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->foreignIdFor(Book::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('book_category');
     }
 };
--- database/migrations/2025_02_04_012157_create_author_book_table.php
+++ database/migrations/2025_02_04_012157_create_author_book_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('author_book', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->foreignIdFor(Author::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Book::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('author_book');
     }
 };
