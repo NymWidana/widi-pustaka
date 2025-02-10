68e8b48da054126c80d7d7dd1a8c2dff7b8e2f6f
NymWidana <widan0001@gmail.com>
2025-02-10 19:12:50 +0800

    feat(api): create BookController and define API routes
    - Created API controller for 'Book' in 'app/Http/Controllers/API/BookController.php'
    - Defined the corresponding API routes for 'Book' in the routes/api.php file
    
    These changes add the BookController and define the necessary API routes to interact with the Book API controller.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\API\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//routes for book CRUD api
+Route::controller(BookController::class)->group( function(){
+    Route::get('/books', 'index')->name('book.index');
+    Route::post('/books', 'store')->name('book.store');
+    Route::get('/books/{book}', 'show')->name('book.show');
+    Route::put('/books/{book}', 'update')->name('book.update');
+    Route::delete('/books/{book}', 'destroy')->name('book.destroy');
+});
