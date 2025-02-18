b51b89d4de99e0cabac949ba90984ef516568fa7
NymWidana <widan0001@gmail.com>
2025-02-18 10:55:22 +0800

    feat(api): implement BookController with CRUD operations
    - Display a list of books with their authors and categories in the index method
    - Store a new book with associated authors and categories in the store method
    - Show details of a specific book with authors and categories in the show method
    - Update an existing book and its relationships in the update method
    - Delete a book and its relationships in the destroy method
    - Handle exceptions and return appropriate JSON responses for each action
    
    These changes ensure the BookController handles all CRUD operations with proper relationships and error handling.

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -13,14 +19,80 @@ class BookController extends Controller
     public function index()
     {
         //
+        try{
+            $books = Book::with(['authors', 'categories'])->get();
+            return response()->json([
+                'code' => 200,
+                'data' => $books,
+                'success' => True,
+                'message' => 'Successfully retrieved all books data!'
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'code' => 500,
+                'data' => NULL,
+                'success' => False,
+                'message' => 'Internal Server Error! Failed to retrieve books data!'
+            ], 500);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBookRequest $request)
     {
         //
+        try{
+            $validatedData = $request->validated();
+            $book = Book::create([
+                "title" => $validatedData['title'],
+                "description" => $validatedData['description']
+            ]); 
+
+            $categoryIds = [];
+            foreach ($validatedData['categories'] as $categoryName) {
+                $category = Category::where('name', $categoryName)->first();
+                if($category){   
+                    $categoryIds[] = $category->id;
+                }else{
+                    return response()->json([
+                        'code' => 400,
+                        'success' => False,
+                        'message' => 'Failed saving book data!, the specified book category is not found'
+                    ], 400);
+                }
+            }
+            $authorIds = [];
+            foreach ($validatedData['authors'] as $authorName) {
+                $author = Author::firstOrCreate(['name' => $authorName]);
+                $authorIds[] = $author->id;
+            }
+            $book->authors()->sync($authorIds);
+            $book->categories()->sync($categoryIds);
+            if($book){
+                return response()->json([
+                    'code' => 201,
+                    'data' => $book,
+                    'success' => True,
+                    'message' => 'Successfully saved a book data!'
+                ], 201);
+            }
+            else{
+                return response()->json([
+                    'code' => 500,
+                    'success' => False,
+                    'message' => 'Failed to save the book data!'
+                ], 500);
+            }
+        }catch(Exception $e){
+            return response()->json([
+                'code' => 500,
+                'success' => False,
+                'message' => 'Failed to save book data. Internal Server error! ' . $e->getMessage(),
+                'e' => $e
+            ], 500);
+        }
     }
 
     /**
@@ -29,14 +101,95 @@ class BookController extends Controller
     public function show(string $id)
     {
         //
+        try{
+            $book = Book::find($id);
+            if(!$book){
+                return response()->json([
+                'code' => 404,
+                'data' => NULL,
+                'success' => False,
+                'message' => 'Failed to retrieve the book data. The book data is not found!'
+            ], 404);
+            }
+            return response()->json([
+                'code' => 200,
+                'data' => $book,
+                'success' => True,
+                'message' => 'Successfully retrieved the book data!'
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'code' => 500,
+                'data' => NULL,
+                'success' => False,
+                'message' => 'Failed to retrieve the book data. Internal Server Error!'. $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBookRequest $request, string $id)
     {
         //
+        try{
+            $book = Book::find($id);
+            if(!$book){
+                return response()->json([
+                    'code' => 404,
+                    'success' => False,
+                    'message' => 'Failed to retrieve the book data. The book data is not found!'
+                ], 404);
+            }
+            $validatedData = $request->validated();
+            $updated = $book->update([
+                "title" => $validatedData['title'],
+                "description" => $validatedData['description']
+            ]); 
+
+            $categoryIds = [];
+            foreach ($validatedData['categories'] as $categoryName) {
+                $category = Category::where('name', $categoryName)->first();
+                if($category){   
+                    $categoryIds[] = $category->id;
+                }else{
+                    return response()->json([
+                        'code' => 400,
+                        'success' => False,
+                        'message' => 'Failed to update book data!, the specified book category is not found'
+                    ], 400);
+                }
+            }
+            $authorIds = [];
+            foreach ($validatedData['authors'] as $authorName) {
+                $author = Author::firstOrCreate(['name' => $authorName]);
+                $authorIds[] = $author->id;
+            }
+            $book->authors()->sync($authorIds);
+            $book->categories()->sync($categoryIds);
+            if($updated){
+                return response()->json([
+                    'code' => 201,
+                    'data' => Book::with(['authors', 'categories'])->find($id),
+                    'success' => True,
+                    'message' => 'Successfully updated a book data!'
+                ], 201);
+            }
+            else{
+                return response()->json([
+                    'code' => 500,
+                    'success' => False,
+                    'message' => 'Failed to update the book data!'
+                ], 500);
+            }
+        }catch(Exception $e){
+            return response()->json([
+                'code' => 500,
+                'success' => False,
+                'message' => 'Failed to update the book data. Internal Server Error!'. $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -45,5 +198,37 @@ class BookController extends Controller
     public function destroy(string $id)
     {
         //
+        try{
+            $book = Book::find($id);
+            if(!$book){
+                return response()->json([
+                    'code' => 404,
+                    'success' => False,
+                    'message' => 'Failed to retrieve the book data. The book data is not found!'
+                ], 404);
+            }
+            $deleted = $book->delete();
+            if($deleted){
+                return response()->json([
+                    'code' => 200,
+                    'data' => $book,
+                    'success' => True,
+                    'message' => 'Successfully deleted a book data!'
+                ], 200);
+            }
+            else{
+                return response()->json([
+                    'code' => 500,
+                    'success' => false,
+                    'message' => 'Failed to delete the book data!'
+                ], 500);
+            }
+        }catch(Exception $e){
+            return response()->json([
+                'code' => 500,
+                'success' => False,
+                'message' => 'Failed to delete the book data. Internal Server Error!'. $e->getMessage()
+            ], 500);
+        }
     }
 }
