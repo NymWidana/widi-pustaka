61a355c151479b53a181334a1e47c68fd3984b7a
NymWidana <widan0001@gmail.com>
2025-02-18 10:20:55 +0800

    feat(validation): add validation rules, preparation, messages and custom error handling for StoreBookRequest and UpdateBookRequest
    - Set up validation rules in StoreBookRequest and UpdateBookRequest
    - Added prepareForValidation to preprocess input data
    - Added custom validation messages
    - Created custom validation rules (NotEmptyArray)
    - Customized API validation error response in failedValidation
    
    These changes ensure the StoreBookRequest and UpdateBookRequest class has solid validation, preprocesses input data, provides clear error messages, includes custom rules, and improves error handling for API responses.

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\NotEmptyArray;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreBookRequest extends FormRequest
 {
@@ -11,7 +14,7 @@ class StoreBookRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +25,108 @@ class StoreBookRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'string',
+            'categories' => 'array',
+            'categories.*' => [
+                'string',
+                'exists:categories,name'
+            ],
+            'authors' => [
+                'required',
+                'array',
+                new NotEmptyArray
+            ],
+            'author.*' => 'string'
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        //makes sure intended string data is turned into array
+        //if its neither string or array, it will throw validation error by using validation rules 'array'
+        //if its array of non string value, it will throw validation error by using validation rule to each of the array items 'string'
+
+        $authors = $this->input('authors');
+        $categories = $this->input('categories');
+        if (is_string($authors)) {
+            $this->merge([
+                'authors' => [$authors]
+            ]);
+        }
+        if (is_string($categories)) {
+            $this->merge([
+                'categories' => [strtolower($categories)]
+            ]);
+        }
+        else if (is_array($categories)){
+            $lowerCasedCategory = [];
+            foreach ($categories as $category) {
+                if(is_string($category)){
+                    $lowerCasedCategory[]= strtolower($category);
+                }
+                else{
+                    $lowerCasedCategory[]= $category;
+                }
+            }
+            $this->merge([
+                'categories' => $lowerCasedCategory
+            ]);
+        }
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        if ($this->expectsJson()) {
+            $errors = $validator->errors();
+
+            $firstError = $errors->first();
+            $errorCount = $errors->count();
+            $message = $errorCount > 1
+                ? $firstError . '(and '. $errorCount - 1 .' more errors)'
+                : $firstError;
+            $response = response()->json([
+                'code' => 422,
+                'success' => false,
+                'message' => $message,
+                'errors' => $validator->errors()
+            ], 422);
+
+            throw new HttpResponseException($response);
+        } else {
+            parent::failedValidation($validator);
+        }
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'Book title is required.',
+            'title.string' => 'Book title must be a valid string.',
+            'title.max' => 'Book title must not exceed 255 characters.',
+            'description.string' => 'Book description must be a valid string.',
+            'categories.array' => 'The book categories must be a string or an array',
+            'categories.*.string' => 'Each book categories must be a valid string.',
+            'categories.*.exists' => 'One or more selected book categories do not exist.',
+            'authors.required' => 'Book authors is required.'
         ];
     }
 }
