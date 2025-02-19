521aab4fabc9815df15d073c6544a805361db411
NymWidana <widan0001@gmail.com>
2025-02-19 20:03:54 +0800

    feat(api): add form requests and controllers for author and category management
    - Created AuthorRequest and CategoryRequest form request classes
    - Implemented validation rules, custom messages, and custom failed validation in form request classes
    - Created AuthorController and CategoryController for managing author and category API
    
    These changes introduce form request classes with validation and error handling, and provide dedicated controllers for efficient management of author and category.

---
+++ app/Http/Requests/UpdateAuthorRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'string'
+        ];
+    }
+        protected function failedValidation(Validator $validator)
+    {
+        if ($this->expectsJson()) {
+            $errors = $validator->errors();
+
+            $firstError = $errors->first();
+            $errorCount = $errors->count();
+            $message = $errorCount > 1
+                ? $firstError . '(and '. $errorCount - 1 .' more errors)'
+                : $firstError;
+            $response = response()->json([
+                'code' => 422,
+                'success' => false,
+                'message' => $message,
+                'errors' => $validator->errors()
+            ], 422);
+
+            throw new HttpResponseException($response);
+        } else {
+            parent::failedValidation($validator);
+        }
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.string' => 'Author name must be a valid string.',
+        ];
+    }
+
+}
